1457b61d72817fb2c8ad048f63e04ddbf8016b5e
MrSallivan <nik.sallivan@gmail.com>
2023-11-30 14:06:03 +1000

    start

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>My Hello!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,7 +17,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
