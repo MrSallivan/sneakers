b7be6715e77aba95ec6c06e3e4898f891ff8226e
MrSallivan <nik.sallivan@gmail.com>
2023-12-14 23:32:20 +1000

    3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import Card from "./components/Card"
 import Header from "./components/Header"
 import Drawer from "./components/Drawer"
 
+const arr = [
+  { name: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999 , image: "../img/sneakers/1.jpg"},
+  { name: "Мужские Кроссовки Nike Air Max 270", price: 11500 , image: "../img/sneakers/2.jpg"},
+  { name: "Мужские Кроссовки Nike Blazer Mid Suede", price: 8499, image: "../img/sneakers/3.jpg" },
+  { name: "Кроссовки Puma X Aka Boku Future Rider", price: 8999 , image: "../img/sneakers/4.jpg"},
+  { name: "Мужские Кроссовки Under Armour Curry 8", price: 15199 , image: "../img/sneakers/5.jpg"},
+  { name: "Мужские Кроссовки Nike Kyrie 7", price: 11299, image: "../img/sneakers/6.jpg"},
+  { name: "Мужские Кроссовки Jordan Air Jordan 11", price: 10799 , image: "../img/sneakers/7.jpg"},
+  { name: "Мужские Кроссовки Nike LeBron XVIII", price: 16499 , image: "../img/sneakers/8.jpg"},
+  { name: "Мужские Кроссовки Nike Lebron XVIII Low", price: 13999 , image: "../img/sneakers/9.jpg"},
+  { name: "Мужские Кроссовки Nike Kyrie Flytrap IV", price: 11299 , image: "../img/sneakers/12.jpg"}
+]
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -17,10 +30,9 @@ function App() {
           </div>
         </div>
         <div className="d-flex mr-30">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((item, i) => (
+            <Card key={i} title ={item.name} price = {item.price} image={item.image} />
+          ))}
         </div>
       </div>
     </div>
@@ -28,4 +40,3 @@ function App() {
 }
 
 export default App
-
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,17 @@
 import React from "react"
 
-const Card = () => {
+const Card = ({ title, price, image }) => {
   return (
     <div className="card">
       <div className="favorite">
         <img src="../img/heart-unliked.svg" alt="favorite" />
       </div>
-      <img
-        width={133}
-        height={112}
-        src="../img/sneakers/1.jpg"
-        alt="Sneakers"
-      />
-      <h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={image} alt="Sneakers" />
+      <h5>{title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column ">
           <span>Цена: </span>
-          <b>12 999 руб.</b>
+          <b>{price} руб.</b>
         </div>
         <button className="button">
           <img src="../img/plus.svg" alt="plus button" width={11} height={11} />
